4a722547b5cae0b368e8206f408ee68a1ce97d40
david <ddb048@gmail.com>
2023-02-07 20:39:05 -0800

    updated prob 4

--- febHackerRank4.py
+++ febHackerRank4.py
@@ -2,13 +2,20 @@
 
 # NOTE - determine the max positive spread for a sotck given its price history.  If the stock remains flat or declines for the full period, return -1.
 
-def maxDifference(array):
-    possibleHighest = [-1]
+def maxDifference(a):
+    vmin = a[0]
+    dmax = 0
 
-    for x in array:
-        for y in array:
-            if y > x:
-                possibleHighest.append(y - x)
+    for i in range(len(a)):
+        if (a[i] < vmin):
+            vmin = a[i]
+        elif (a[i] - vmin > dmax):
+            dmax = a[i] - vmin
+
+    if dmax == 0:
+        dmax = -1
+    return dmax
+
 
 # loop through the array in double nested
 # in i, j, if j is > i, push difference into possibleHighest
